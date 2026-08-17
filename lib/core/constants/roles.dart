bb3f8bd3e210f638585c1ef65304bc32ae456074
alyusef <yumbaportada@gmail.com>
2026-08-17 17:55:37 -0400

    Subiendo carpeta android

--- lib/core/constants/roles.dart
+++
@@ -1,6 +0,0 @@
-class Roles {
-  static const String superAdmin = 'superadmin';
-  static const String admin = 'admin';
-  static const String cashier = 'dependiente';
-  static const String warehouse = 'almacenero';
-}
--- lib/core/modules.dart
+++
@@ -1,8 +0,0 @@
-class Modules {
-
-  static const String pos = "POS";
-  static const String inventory = "INVENTORY";
-  static const String reports = "REPORTS";
-  static const String users = "USERS";
-
-}
